ffeb5024401701b085d4e20f01fd7ccc23d198f4
hmigl <88746406+hmigl@users.noreply.github.com>
2023-02-24 13:08:45 -0300

    refactor: break 'vectors_setup()' into smaller functions

--- src/setup/vectors_setup.c
+++ src/setup/vectors_setup.c
@@ -1,9 +1,19 @@
 #include "cub3d.h"
 
-void	vectors_setup(t_game *game)
+static void	setup_camera_plane(t_game *game)
+{
+	game->plane.x = 0;
+	game->plane.y = 0.66;
+}
+
+static void	setup_facing_direction(t_game *game)
 {
 	game->direction.x = -1;
 	game->direction.y = 0;
-	game->plane.x = 0;
-	game->plane.y = 0.66;
+}
+
+void	vectors_setup(t_game *game)
+{
+	setup_facing_direction(game);
+	setup_camera_plane(game);
 }
