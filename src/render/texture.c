3b24e74673a163fc273d6e9d3274044237260aae
Henrique <hpcavalcante@gmail.com>
2023-02-28 22:36:11 -0300

    refactor insert_texture

--- src/render/texture.c
+++ src/render/texture.c
@@ -37,12 +37,12 @@ void	insert_texture(t_game *game, int x)
 	game->rc.wall.x -= (int)game->rc.wall.x;
 	init_texture(game);
 	set_texture(game);
-	game->rc.tex.x = (int)(game->rc.wall.x * (double)game->rc.texture.width);
+	game->rc.tex.x = (int)(game->rc.wall.x * (double)game->rc.texture->width);
 	if (game->rc.side == 0 && game->rc.ray_dir.x > 0)
-		game->rc.tex.x = game->rc.texture.width - game->rc.tex.x - 1;
+		game->rc.tex.x = game->rc.texture->width - game->rc.tex.x - 1;
 	if (game->rc.side == 1 && game->rc.ray_dir.y < 0)
-		game->rc.tex.x = game->rc.texture.width - game->rc.tex.x - 1;
-	game->rc.tex_step = 1.0 * game->rc.texture.height / game->rc.line_height;
+		game->rc.tex.x = game->rc.texture->width - game->rc.tex.x - 1;
+	game->rc.tex_step = 1.0 * game->rc.texture->height / game->rc.line_height;
 	game->rc.tex_pos = (game->rc.draw_start - WIN_HEIGHT
 			/ 2 + game->rc.line_height / 2) * game->rc.tex_step;
 	//texture_loop(game, x);
