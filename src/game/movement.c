e79d3476f2608c0cdb423d6522bae32f3c76cbde
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-10 16:43:17 -0300

    style: follow norme

--- src/game/movement.c
+++ src/game/movement.c
@@ -2,143 +2,102 @@
 
 static void	move_right(t_game *game)
 {
-	double		next_x;
-	double		next_y;
-	char	**map;
-	double	move_x;
-	double	move_y;
+	t_player	*p;
+	char		**map;
 
 	map = game->scene->map.map_as_2d_array;
-	move_x = game->player.dir.y * game->player.move_speed;
-	move_y = -game->player.dir.x * game->player.move_speed;
-	next_x = game->player.pos.x + move_x
-		+ copysign(WALL_COLLISION_THRESHOLD, move_x);
-	next_y = game->player.pos.y + move_y
-		+ copysign(WALL_COLLISION_THRESHOLD, move_y);
-	if (map[(int)game->player.pos.x][(int)game->player.pos.y] != FLOOR)
-		return;
-
-	// Check for collision at adjacent positions
-	if (map[(int)next_x][(int)game->player.pos.y] != FLOOR)
-		move_x = 0;
-	if (map[(int)game->player.pos.x][(int)next_y] != FLOOR)
-		move_y = 0;
-
-	// Check for diagonal collisions
-	if (map[(int)next_x][(int)next_y] != FLOOR)
-		return;
-
-	game->player.pos.x += move_x;
-	game->player.pos.y += move_y;
-	// if (map[(int)next_x][(int)game->player.pos.y] == FLOOR)
-	// 	game->player.pos.x += move_x;
-	// if (map[(int)game->player.pos.x][(int)next_y] == FLOOR)
-	// 	game->player.pos.y += move_y;
+	p = &(game->player);
+	p->move_x = p->dir.y * p->move_speed;
+	p->move_y = -p->dir.x * p->move_speed;
+	p->next_x = p->pos.x + p->move_x
+		+ copysign(WALL_COLLISION_THRESHOLD, p->move_x);
+	p->next_y = p->pos.y + p->move_y
+		+ copysign(WALL_COLLISION_THRESHOLD, p->move_y);
+	if (map[(int)p->pos.x][(int)p->pos.y] != FLOOR)
+		return ;
+	if (map[(int)p->next_x][(int)p->pos.y] != FLOOR)
+		p->move_x = 0;
+	if (map[(int)p->pos.x][(int)p->next_y] != FLOOR)
+		p->move_y = 0;
+	if (map[(int)p->next_x][(int)p->next_y] != FLOOR)
+		return ;
+	p->pos.x += p->move_x;
+	p->pos.y += p->move_y;
 }
 
 static void	move_left(t_game *game)
 {
-	double		next_x;
-	double		next_y;
-	char	**map;
-	double	move_x;
-	double	move_y;
+	t_player	*p;
+	char		**map;
 
 	map = game->scene->map.map_as_2d_array;
-	move_x = -game->player.dir.y * game->player.move_speed;
-	move_y = game->player.dir.x * game->player.move_speed;
-	next_x = game->player.pos.x + move_x
-		+ copysign(WALL_COLLISION_THRESHOLD, move_x);
-	next_y = game->player.pos.y + move_y
-		+ copysign(WALL_COLLISION_THRESHOLD, move_y);
-	if (map[(int)game->player.pos.x][(int)game->player.pos.y] != FLOOR)
-		return;
-
-	// Check for collision at adjacent positions
-	if (map[(int)next_x][(int)game->player.pos.y] != FLOOR)
-		move_x = 0;
-	if (map[(int)game->player.pos.x][(int)next_y] != FLOOR)
-		move_y = 0;
-
-	// Check for diagonal collisions
-	if (map[(int)next_x][(int)next_y] != FLOOR)
-		return;
-
-	game->player.pos.x += move_x;
-	game->player.pos.y += move_y;
-	// if (map[(int)next_x][(int)game->player.pos.y] == FLOOR)
-	// 	game->player.pos.x += move_x;
-	// if (map[(int)game->player.pos.x][(int)next_y] == FLOOR)
-	// 	game->player.pos.y += move_y;
+	p = &(game->player);
+	p->move_x = -p->dir.y * p->move_speed;
+	p->move_y = p->dir.x * p->move_speed;
+	p->next_x = p->pos.x + p->move_x
+		+ copysign(WALL_COLLISION_THRESHOLD, p->move_x);
+	p->next_y = p->pos.y + p->move_y
+		+ copysign(WALL_COLLISION_THRESHOLD, p->move_y);
+	if (map[(int)p->pos.x][(int)p->pos.y] != FLOOR)
+		return ;
+	if (map[(int)p->next_x][(int)p->pos.y] != FLOOR)
+		p->move_x = 0;
+	if (map[(int)p->pos.x][(int)p->next_y] != FLOOR)
+		p->move_y = 0;
+	if (map[(int)p->next_x][(int)p->next_y] != FLOOR)
+		return ;
+	p->pos.x += p->move_x;
+	p->pos.y += p->move_y;
 }
 
 static void	move_backward(t_game *game)
 {
-	double	next_x;
-	double	next_y;
-	char	**map;
-	double	move_x;
-	double	move_y;
+	t_player	*p;
+	char		**map;
 
 	map = game->scene->map.map_as_2d_array;
-	move_x = -game->player.dir.x * game->player.move_speed;
-	move_y = -game->player.dir.y * game->player.move_speed;
-	next_x = game->player.pos.x + move_x
-		+ copysign(WALL_COLLISION_THRESHOLD, move_x);
-	next_y = game->player.pos.y + move_y
-		+ copysign(WALL_COLLISION_THRESHOLD, move_y);
-
-	if (map[(int)game->player.pos.x][(int)game->player.pos.y] != FLOOR)
-		return;
-
-	// Check for collision at adjacent positions
-	if (map[(int)next_x][(int)game->player.pos.y] != FLOOR)
-		move_x = 0;
-	if (map[(int)game->player.pos.x][(int)next_y] != FLOOR)
-		move_y = 0;
-
-	// Check for diagonal collisions
-	if (map[(int)next_x][(int)next_y] != FLOOR)
-		return;
-
-	game->player.pos.x += move_x;
-	game->player.pos.y += move_y;
-	// if (map[(int)next_x][(int)game->player.pos.y] == FLOOR)
-	//	game->player.pos.x += move_x;
-	// if (map[(int)game->player.pos.x][(int)next_y] == FLOOR)
-	//	game->player.pos.y += move_y;
+	p = &(game->player);
+	p->move_x = -p->dir.x * p->move_speed;
+	p->move_y = -p->dir.y * p->move_speed;
+	p->next_x = p->pos.x + p->move_x
+		+ copysign(WALL_COLLISION_THRESHOLD, p->move_x);
+	p->next_y = p->pos.y + p->move_y
+		+ copysign(WALL_COLLISION_THRESHOLD, p->move_y);
+	if (map[(int)p->pos.x][(int)p->pos.y] != FLOOR)
+		return ;
+	if (map[(int)p->next_x][(int)p->pos.y] != FLOOR)
+		p->move_x = 0;
+	if (map[(int)p->pos.x][(int)p->next_y] != FLOOR)
+		p->move_y = 0;
+	if (map[(int)p->next_x][(int)p->next_y] != FLOOR)
+		return ;
+	p->pos.x += p->move_x;
+	p->pos.y += p->move_y;
 }
 
-static void move_forward(t_game *game)
+static void	move_forward(t_game *game)
 {
-	double	next_x;
-	double	next_y;
-	char	**map;
-	double	move_x;
-	double	move_y;
+	t_player	*p;
+	char		**map;
 
 	map = game->scene->map.map_as_2d_array;
-	move_x = game->player.dir.x * game->player.move_speed;
-	move_y = game->player.dir.y * game->player.move_speed;
-	next_x = game->player.pos.x + move_x + copysign(WALL_COLLISION_THRESHOLD, move_x);
-	next_y = game->player.pos.y + move_y + copysign(WALL_COLLISION_THRESHOLD, move_y);
-
-	// Check for collision at current position
-	if (map[(int)game->player.pos.x][(int)game->player.pos.y] != FLOOR)
-		return;
-
-	// Check for collision at adjacent positions
-	if (map[(int)next_x][(int)game->player.pos.y] != FLOOR)
-		move_x = 0;
-	if (map[(int)game->player.pos.x][(int)next_y] != FLOOR)
-		move_y = 0;
-
-	// Check for diagonal collisions
-	if (map[(int)next_x][(int)next_y] != FLOOR)
-		return;
-
-	game->player.pos.x += move_x;
-	game->player.pos.y += move_y;
+	p = &(game->player);
+	p->move_x = p->dir.x * p->move_speed;
+	p->move_y = p->dir.y * p->move_speed;
+	p->next_x = p->pos.x + p->move_x
+		+ copysign(WALL_COLLISION_THRESHOLD, p->move_x);
+	p->next_y = p->pos.y + p->move_y
+		+ copysign(WALL_COLLISION_THRESHOLD, p->move_y);
+	if (map[(int)p->pos.x][(int)p->pos.y] != FLOOR)
+		return ;
+	if (map[(int)p->next_x][(int)p->pos.y] != FLOOR)
+		p->move_x = 0;
+	if (map[(int)p->pos.x][(int)p->next_y] != FLOOR)
+		p->move_y = 0;
+	if (map[(int)p->next_x][(int)p->next_y] != FLOOR)
+		return ;
+	p->pos.x += p->move_x;
+	p->pos.y += p->move_y;
 }
 
 void	move_player(t_game *game)
